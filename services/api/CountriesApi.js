b153f5c23a3eb2dec31828094af850004845f79c
Flavia <flavia_ferri@hotmail.com>
2020-12-22 15:15:41 +0100

    Create axios instance

--- services/api/CountriesApi.js
+++ services/api/CountriesApi.js
@@ -1,27 +1,26 @@
 import axios from 'axios'
 
+const axiosInstance = axios.create({
+  baseURL: 'https://restcountries.eu/rest/v2/',
+  timeout: 3000,
+})
+
 export default {
   getCountry(country) {
-    return axios
-      .get(`https://restcountries.eu/rest/v2/alpha/${country}`)
-      .then((response) => {
-        return response.data
-      })
+    return axiosInstance.get(`alpha/${country}`).then((response) => {
+      return response.data
+    })
   },
 
   getAllCountries() {
-    return axios
-      .get(`https://restcountries.eu/rest/v2/all`)
-      .then((response) => {
-        return response.data
-      })
+    return axiosInstance.get(`all`).then((response) => {
+      return response.data
+    })
   },
 
   getCountries(region) {
-    return axios
-      .get(`https://restcountries.eu/rest/v2/region/${region}`)
-      .then((response) => {
-        return response.data
-      })
+    return axiosInstance.get(`region/${region}`).then((response) => {
+      return response.data
+    })
   },
 }
